16efaba53bca32822afd2ce7d966e5129d69e230
miirko <mirkoo@posteo.org>
2018-04-25 15:24:19 +0200

    created separate class for inserting data at startup

--- src/test/java/de/htwg/swqs/shopui/cucumber/CucumberTestExample.java
+++ src/test/java/de/htwg/swqs/shopui/cucumber/CucumberTestExample.java
@@ -5,14 +5,14 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(Cucumber.class)
+@RunWith(cucumber.api.junit.Cucumber.class)
 @SpringBootTest
 @CucumberOptions(
     features = "classpath:cucumber",
     plugin = "pretty",
     strict = true
 )
-public class CucumberTest {
+public class CucumberTestExample {
 
 }
 
