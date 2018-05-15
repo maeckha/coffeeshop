8d4081fd1a00436f4083c1bb8f84736323961153
Mirko Bay <mibay@htwg-konstanz.de>
2018-05-15 21:12:21 +0200

    removed the whole selenium stuff in maven and gitlab ci...doesnt work so simply

--- src/test/java/de/htwg/swqs/shopui/selenium/SeleniumEndToEnd.java
+++ src/test/java/de/htwg/swqs/shopui/selenium/SeleniumEndToEnd.java
@@ -36,7 +36,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
-public class SeleniumEndToEndTest {
+public class SeleniumEndToEnd {
 
   @LocalServerPort
   private int port;
