4ce81598402a0d4af8d8885a004e035cea35fefe
Markus Eiglsperger <meiglspe@htwg-konstanz.de>
2018-11-30 17:09:48 +0100

    Changed POM and about.html to show version number.

--- aws-eb.sh
+++ aws-eb.sh
@@ -1,22 +1,60 @@
-## Beanstalk
+## Test Environment
+####################
 
-eb create shop-test -p java --single \
-    --envvars SERVER_PORT=5000,SPRING_DATASOURCE_USERNAME=swqsuser,SPRING_DATASOURCE_PASSWORD=swqsswqs,SPRING_DATASOURCE_URL=jdbc:mysql://shop-db.ch2g9nokdg1u.us-west-2.rds.amazonaws.com:3306/swqsdemo,SPRING_JPA_PROPERTIES_HIBERNATE_DIALECT=org.hibernate.dialect.MySQL5Dialect,SPRING_DATASOURCE_DRIVER-CLASS-NAME=com.mysql.jdbc.Driver
+# create
 
-eb deploy
+eb create CoffeeShop-env-test -p java --single \
+    --envvars SERVER_PORT=5000,SPRING_DATASOURCE_USERNAME=admin,SPRING_DATASOURCE_PASSWORD=_test42,SPRING_DATASOURCE_URL=jdbc:mysql://coffeeshop-test-db.ch2g9nokdg1u.us-west-2.rds.amazonaws.com:3306/coffeeshop,SPRING_JPA_PROPERTIES_HIBERNATE_DIALECT=org.hibernate.dialect.MySQL5Dialect,SPRING_DATASOURCE_DRIVER-CLASS-NAME=com.mysql.jdbc.Driver
 
-eb terminate shop-test --force
+aws rds create-db-instance --db-name coffeeshop \
+    --db-instance-identifier  coffeeshop-test-db --db-instance-class db.t2.micro --engine mysql \
+    --allocated-storage 5 --master-username admin --master-user-password _test42
 
-## DB
-
-aws rds create-db-instance --db-name swqsdemo \
-    --db-instance-identifier  shop-db --db-instance-class db.t2.micro --engine mysql \
-    --allocated-storage 5 --master-username swqsuser --master-user-password swqsswqs
-
-aws rds describe-db-instances
-
-aws rds delete-db-instance --db-instance-identifier shop-db --skip-final-snapshot
+# operate
 
 mysql -h shop-db.ch2g9nokdg1u.us-west-2.rds.amazonaws.com -u swqsuser -p swqsdemo
 
 http://shop-test.8qvzgknme3.us-west-2.elasticbeanstalk.com
+
+eb deploy
+
+aws rds describe-db-instances
+
+# destroy
+
+eb terminate CoffeeShop-env-test --force
+
+aws rds delete-db-instance --db-instance-identifier shop-db --skip-final-snapshot
+
+
+
+## Staging Environment
+#######################
+
+# create
+
+
+aws rds create-db-instance --db-name coffeeshop \
+    --db-instance-identifier  coffeeshop-staging-db --db-instance-class db.t2.micro --engine mysql \
+    --allocated-storage 5 --master-username admin --master-user-password stage_42
+
+eb create CoffeeShop-env-staging -p java --single \
+    --envvars SERVER_PORT=5000,SPRING_DATASOURCE_USERNAME=admin,SPRING_DATASOURCE_PASSWORD=stage_42,SPRING_DATASOURCE_URL=jdbc:mysql://coffeeshop-staging-db.ch2g9nokdg1u.us-west-2.rds.amazonaws.com:3306/coffeeshop,SPRING_JPA_PROPERTIES_HIBERNATE_DIALECT=org.hibernate.dialect.MySQL5Dialect,SPRING_DATASOURCE_DRIVER-CLASS-NAME=com.mysql.jdbc.Driver
+
+# destroy
+
+aws rds delete-db-instance --db-instance-identifier coffeeshop-staging-db --skip-final-snapshot
+
+eb terminate CoffeeShop-env-staging --force
+
+
+
+
+
+## Production Environment
+##########################
+
+
+eb create CoffeeShop-env-production -p java --single \
+    --envvars SERVER_PORT=5000,SPRING_DATASOURCE_USERNAME=swqsuser,SPRING_DATASOURCE_PASSWORD=swqsswqs,SPRING_DATASOURCE_URL=jdbc:mysql://shop-db.ch2g9nokdg1u.us-west-2.rds.amazonaws.com:3306/swqsdemo,SPRING_JPA_PROPERTIES_HIBERNATE_DIALECT=org.hibernate.dialect.MySQL5Dialect,SPRING_DATASOURCE_DRIVER-CLASS-NAME=com.mysql.jdbc.Driver
+
