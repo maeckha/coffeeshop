615b9bb4dc695a0f41acf7e53049dc3663f9b1e7
Markus Eiglsperger <meiglspe@htwg-konstanz.de>
2019-12-18 11:39:09 +0100

    Fixed shopping cart access bug.

--- src/main/java/de/htwg/swqs/shopui/util/WebSecurityConfig.java
+++ src/main/java/de/htwg/swqs/shopui/util/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/products/*","/carts/*","/show-cart","/order","/about/","/submit-order","/**/*.css","/**/*.js","/img/*.jpg").permitAll()
+                .antMatchers("/", "/products/*","/carts/*","/show-cart/","/order","/about/","/submit-order","/**/*.css","/**/*.js","/img/*.jpg").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
