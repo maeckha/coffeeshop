dc320557841d581fd215b3ad342e61f13a3604ac
meck <markus.eckhardt@htwg-konstanz.de>
2020-07-01 10:10:12 +0200

    started implementation of ermMwstUmrechnung

---
+++ src/test/java/de/htwg/swqs/order/model/ermMwstUmrechnungTest.java
@@ -0,0 +1,16 @@
+package de.htwg.swqs.order.model;
+
+import junit.framework.TestCase;
+
+public class ermMwstUmrechnungTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testUmrechnung() {
+    }
+}
